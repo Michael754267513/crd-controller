21b4f3998c147bd88c6b7ed50265ef0b818b3c54
hzeng <754267513@qq.com>
2020-06-02 13:46:50 +0800

    控制器 kind Spec 参数列表

---
+++ api/v1/handpay_types.go
@@ -0,0 +1,67 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// HandpaySpec defines the desired state of Handpay
+// handpay kind Spec 定义参数 列表
+type HandpaySpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of Handpay. Edit Handpay_types.go to remove/update
+	// 添加 所需要的yaml 变量
+	Image    string `json:"image,omitempty"` // json 解析 filed名用image
+	Replicas int    `json:"replicas,omitempty"`
+}
+
+// HandpayStatus defines the observed state of Handpay
+type HandpayStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	Status string `json:"status"`
+}
+
+// +kubebuilder:object:root=true
+
+// Handpay is the Schema for the handpays API
+type Handpay struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   HandpaySpec   `json:"spec,omitempty"`
+	Status HandpayStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:subresource:status
+// +kubebuilder:object:root=true
+
+// HandpayList contains a list of Handpay
+type HandpayList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Handpay `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Handpay{}, &HandpayList{})
+}
